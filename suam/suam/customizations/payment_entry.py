b32bf6b68f10a3ca8aab7b938585b041b612a974
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-07-02 06:01:19 +0300

    feat: Add automatic receipt printing on payment entry submission

---
+++ suam/suam/customizations/payment_entry.py
@@ -0,0 +1,29 @@
+import frappe
+from frappe import _
+
+def on_submit(doc, method):
+    if not doc.custom_kimzone_commission:
+        print_receipt_automatically(doc, method)
+
+def print_receipt_automatically(doc, method):
+    """
+    Print Receipt automatically when payment entry is submitted'.
+    """
+    # Fetch the printer settings document
+    prints  = frappe.get_single("Kimzone Settings")
+
+    printer_name = prints.cashier_printer
+
+    if not printer_name:
+        frappe.log_error(f"No printer settings found for printer: {printer_name}", "Receipt Print Error")
+        return
+
+    # Call the function directly (No background queue)
+    frappe.call(
+        "kimzone.kimzone_ltd.customizations.print_format.print_by_server",
+        doctype=doc.doctype,
+        name=doc.name,
+        printer_setting=printer_name,
+        print_format=prints.customer_credit_payment,
+        no_letterhead=1
+    )
