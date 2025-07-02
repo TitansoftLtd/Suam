6797208f878bd30d85be1542d2903a653febf779
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-07-02 06:02:36 +0300

    feat: Implement apply_doc_workflow function to manage document workflows

---
+++ suam/suam/customizations/workflow.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe import _
+from frappe.model.workflow import apply_workflow
+
+@frappe.whitelist()
+def apply_doc_workflow(doctype, docname, action, comment=None):
+    """
+    Apply Workflow using document type and document name along with action and comment
+    POST Method call <Site URL>/api/method/kimzone.kimzone_ltd.customizations.workflow.apply_doc_workflow
+    Payload:
+        {
+            "doctype": "Item",
+            "docname": "Ago Test for JPL",
+            "action": "Confirm",
+            "comment": "Administrator rejected the document",
+        }
+    """
+    doc = frappe.get_doc(doctype, docname)
+    if comment:
+        frappe.enqueue_doc(doc.doctype, doc.name, "add_comment", text=comment)
+    apply_workflow(doc, action)
+    return doc
