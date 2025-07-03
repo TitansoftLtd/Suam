dd9526a2ceccd4338578c8f734adf342548804fc
Livuza <livuzacharles@gmail.com>
2025-07-03 18:56:03 +0300

    feat: remove Cashier Closing Entry doctype and related files, update Stock Take to reference Chief Cashier Closing Entry

--- suam/public/js/pos_profile.js
+++ suam/public/js/pos_profile.js
@@ -1,13 +1,13 @@
-frappe.ui.form.on('POS Profile', {
-    refresh: function(frm) {
-        frm.set_df_property('warehouse', 'reqd', 0);
-        frm.set_value('warehouse', '');
-    },
-    validate: function(frm) {
-        // Prevent validation for warehouse field
-        if (!frm.doc.warehouse) {
-            frm.fields_dict['warehouse'].df.reqd = 0;
-        }
-        frm.set_value('warehouse', '');
-    }
-});
+// frappe.ui.form.on('POS Profile', {
+//     refresh: function(frm) {
+//     //     frm.set_df_property('warehouse', 'reqd', 0);
+//     //     frm.set_value('warehouse', '');
+//     },
+//     validate: function(frm) {
+//         // Prevent validation for warehouse field
+//         if (!frm.doc.warehouse) {
+//             frm.fields_dict['warehouse'].df.reqd = 0;
+//         }
+//         frm.set_value('warehouse', '');
+//     }
+// });
