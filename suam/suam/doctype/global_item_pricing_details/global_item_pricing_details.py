71f704a6f35ac8d61e6131b9177d54237e120be7
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-07-07 11:13:12 +0300

    feat: add Global Item Pricing Details DocType with initial structure

---
+++ suam/suam/doctype/global_item_pricing_details/global_item_pricing_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Team Web Africa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class GlobalItemPricingDetails(Document):
+	pass
